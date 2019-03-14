b91e5a4aec9a232eb3d5b5836a1165813d973d18
John Ward <johnmartinward@gmail.com>
2019-03-14 19:58:40 +0000

    more lint changes

--- TimeTable/ViewController.swift
+++ TimeTable/ViewController.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    
+
     @IBOutlet weak var pickerView1: UIPickerView!
-    
+
     @IBOutlet weak var pickerView2: UIPickerView!
-    
+
     @IBOutlet weak var labelResult: UILabel!
-    
+
     var timeTable: TimeTable!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+
         pickerView1.selectRow(4, inComponent: 0, animated: false)
         pickerView2.selectRow(4, inComponent: 0, animated: false)
         labelResult.text = "16"
@@ -36,21 +36,20 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return 10
     }
-    
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return "\(row)"
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let val1 = pickerView1.selectedRow(inComponent: 0)
         let val2 = pickerView2.selectedRow(inComponent: 0)
-        
+
         labelResult.text = "\(timeTable.multiply(val1, val2: val2))"
     }
-    
-}
 
+}
